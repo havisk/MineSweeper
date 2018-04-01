0c7e6e4d8a76e5070409f39b5814fbab3c6ce79c
Kool Havis <havisk@gmail.com>
2018-03-31 20:05:54 -0400

    initial commit hardcoded sim board

---
+++ minesweeper.js
@@ -0,0 +1,8 @@
+const blankLine = ' | | ';
+const guessLine = '1| | ';
+const bombLine = ' |B| ';
+
+console.log("This is wha an empty board look like");
+console.log(guessLine);
+console.log(bombLine);
+console.log(blankLine);
