200ce6f7d2fa58268e7bd31977c63ffe2995b96f
Kool Havis <havisk@gmail.com>
2018-04-01 23:46:40 -0400

    created printBoard()

--- minesweeper.js
+++ minesweeper.js
@@ -1,8 +1,20 @@
-const blankLine = ' | | ';
-const guessLine = '1| | ';
-const bombLine = ' |B| ';
+const printBoard = board => {
+  console.log('Current Board: ');
+  console.log(board[0].join(' | '));
+  console.log(board[1].join(' | '));
+  console.log(board[2].join(' | '));
+};
 
-console.log("This is wha an empty board look like");
-console.log(guessLine);
-console.log(bombLine);
-console.log(blankLine);
+let board = [
+  [' ', ' ', ' '],
+  [' ', ' ', ' '], 
+  [' ', ' ', ' ']
+];
+
+printBoard(board);
+
+board[0][1] = '1';
+board[2][2] = 'B';
+
+printBoard(board);
+
